656d3ca9c2bce3d4521c18434301fc1bbcac010d
Mark Laufersweiler <laufers@gmail.com>
2012-04-23 12:46:36 -0500

    Program to locate and publish large disk space users.

---
+++ spacehog.py
@@ -0,0 +1,38 @@
+#!/usr/local/epd/bin/python
+
+import subprocess
+import argparse
+import glob
+
+# check argument for directory else default to current directory
+
+# if len(sys.argv) < 2:
+#     dirname = "." 
+# else:
+#     dirname = sys.argv[1]
+
+parser = argparse.ArgumentParser(description='Check space of users and look for hogs.')
+parser.add_argument('dirname', metavar='DIRNAME', type=str, default='.', nargs='?', 
+                    help='Directory path and glob for searching')
+parser.add_argument('--sizelimit','-l', dest='sizeLimit', type=int, default= 10,
+                    help='size threshold in gigabytes (GB)')
+args = parser.parse_args()
+
+# print args.dirname, args.sizeLimit
+
+files = glob.glob(args.dirname)
+sizeLimit = args.sizeLimit * 1024 * 1024
+ 
+
+users = subprocess.check_output(["du", "-sk"] + files)
+
+# split string into lines
+splithogs = [s.split("\t", 1) for s in users.splitlines()]
+
+#split lines into data
+splithogs = [(int(s[0]), s[1]) for s in splithogs if int(s[0]) > sizeLimit]
+
+splithogs.sort(reverse = True)
+
+for size,dirName in splithogs:
+	print '%d G \t: %s' % (size/1024/1024, dirName)
